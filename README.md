b5241fe27d70c58bc1e70f345fe188ca0496af5c
aminhp93 <minhpn.org.ec@gmail.com>
2016-12-10 17:32:07 -0800

    adding requirements.txt and readme.md

---
+++ README.md
@@ -0,0 +1,10 @@
+	Package:
+
+-  A delivery management tool provided with using api with Django restful framework
+-  Integrated with Django Authentication, Form and Class-Based View.
+-  30% increasing speed using  prefetch/select_related methods through Django Debug Toolbar
+
+	How to install:
+
+-  pip install -r requirements.txt
+-  python manage.py runserver
--- pagkage/settings.py
+++ pagkage/settings.py
@@ -132,8 +132,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
